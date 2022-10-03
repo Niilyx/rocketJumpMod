a3fe20369b274d5c192909db594f2b168fa07e7a
Idir <nait.meddour.idir100@gmail.com>
2022-10-03 15:50:26 +0200

    fuck

---
+++ src/main/java/fr/niilyx/rocketjump/Items.java
@@ -0,0 +1,17 @@
+package fr.niilyx.rocketjump;
+
+import net.minecraft.world.item.Item;
+
+public class Items {
+
+    public static Item ROCKET_JUMPER;
+    public static Item AMMO;
+
+    public static void setup() {
+//        ROCKET_JUMPER = new Item();
+    }
+
+    public static void register() {
+
+    }
+}
