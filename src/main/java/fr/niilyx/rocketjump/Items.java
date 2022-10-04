7ccb5ef7a697e28d1e19682800fa3ab721fdacab
Idir <nait.meddour.idir100@gmail.com>
2022-10-04 12:02:43 +0200

    RJItem

--- src/main/java/fr/niilyx/rocketjump/Items.java
+++ src/main/java/fr/niilyx/rocketjump/Items.java
@@ -1,17 +1,23 @@
 package fr.niilyx.rocketjump;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 
 public class Items {
 
     public static Item ROCKET_JUMPER;
     public static Item AMMO;
 
-    public static void setup() {
-//        ROCKET_JUMPER = new Item();
-    }
-
-    public static void register() {
-
+    @SubscribeEvent
+    public static void register(RegisterEvent e) {
+        e.register(ForgeRegistries.Keys.ITEMS,
+                itemRegisterHelper -> {
+            itemRegisterHelper.register(new ResourceLocation(Main.MODID, "rocket_jumper"), new RJItem(new Item.Properties().stacksTo(1).setNoRepair().tab(CreativeModeTab.TAB_COMBAT)));
+            itemRegisterHelper.register(new ResourceLocation(Main.MODID, "ammo"), new Item(new Item.Properties().stacksTo(128).setNoRepair().tab(CreativeModeTab.TAB_COMBAT)));
+                });
     }
 }
---
+++ src/main/java/fr/niilyx/rocketjump/RJItem.java
@@ -0,0 +1,18 @@
+package fr.niilyx.rocketjump;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class RJItem extends Item {
+
+    public RJItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+
+    @Override
+    public void onUseTick(Level p_41428_, LivingEntity p_41429_, ItemStack p_41430_, int p_41431_) {
+        super.onUseTick(p_41428_, p_41429_, p_41430_, p_41431_);
+    }
+}
